aa67cee57a1b7974e827e371a06dcc3d1b9c6d00
Brandt Westing <bmwesting@gmail.com>
2011-11-18 17:08:30 -0600

    widget works correctly, using glUseProgram(0) disabled the gl shader program.

--- vtkwidget.py
+++ vtkwidget.py
@@ -2,11 +2,11 @@ from kivy.uix.widget import Widget
 from kivy.app import App
 from kivy.clock import Clock, ClockBase
 from kivy.graphics import Color, Rectangle, Callback, Fbo
-from kivy.graphics.opengl import glEnable, GL_DEPTH_TEST, GL_CULL_FACE
 from vtk import *
 from OpenGL.GL import *
+from OpenGL.GLU import *
 
-# set the environment so that special render window is called
+# set the environment so that special renderer is used by vtk
 import os
 os.environ['VTK_RENDERER'] = 'EmbedOpenGL'
 
@@ -20,7 +20,8 @@ mapper.SetInput(cone.GetOutput())
 actor.SetMapper(mapper)
 ren.AddActor(actor)
 ren.SetBackground(0.0, 1.0, 0.0)
-ren.GetActiveCamera().Dolly(.1)
+ren.GetActiveCamera().Dolly(.3)
+ren.GetActiveCamera().Azimuth(0.5)
 renWin.AddRenderer(ren)
 renWin.SetSize(512,512)
 
@@ -37,29 +38,33 @@ class VTKWidget(Widget):
         self.canvas.ask_update()
 
     def setupVTK(self):
-        Clock.schedule_interval(self.updateVTK, 1 / 60.)
+        Clock.schedule_interval(self.updateVTK, 1 / 1.)
         with self.canvas:
-            self.fbo = Fbo(size=(512,512), clear_color=(.3, .3, .3, .8), push_viewport=True)
+            self.fbo = Fbo(size=(512,512), clear_color=(.3, .3, .3, .8), push_viewport=True, with_depthbuffer=True)
             self.size = self.fbo.size
-            Color(1, 1, 0)
+            Color(0, 0, 1)
             Rectangle(pos=self.pos, size=self.size, texture=self.fbo.texture)
+            
             Callback(self.drawVTK, reset_context=True)
             
     def drawVTK(self, instr):
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_CULL_FACE)
         VTKClock.tick()
+        glViewport(0,0,512,512)
         self.fbo.clear_buffer()
         
         #push GL state of Kivy
+        glUseProgram(0)
         glPushAttrib(GL_ALL_ATTRIB_BITS)
         glMatrixMode(GL_PROJECTION)
         glPushMatrix()
         glMatrixMode(GL_MODELVIEW)
         glPushMatrix()
-        
-        #Load identity and render VTK
         glLoadIdentity()
-        renWin.Render()
         
+        renWin.Render()
+
         #pop previous state of Kivy
         glMatrixMode(GL_MODELVIEW)
         glPopMatrix()
@@ -73,3 +78,4 @@ class MyVTKApp(App):
 
 if __name__ == '__main__':
     MyVTKApp().run()
+
